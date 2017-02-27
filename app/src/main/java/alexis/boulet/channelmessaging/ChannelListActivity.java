18ef9adecdeff6f9c387a9338f51867396797cab
kingofzeloose <alexisboulet0@gmail.com>
2017-02-27 11:38:52 +0100

    WIP Fragment : stay layout landscape to make cool

--- app/src/main/java/alexis/boulet/channelmessaging/ChannelListActivity.java
+++ app/src/main/java/alexis/boulet/channelmessaging/ChannelListActivity.java
@@ -1,29 +1,25 @@
 package alexis.boulet.channelmessaging;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import fragmentPackage.MessageFragment;
+import fragmentPackage.ChannelListFragment;
 
 /**
  * Created by bouleta on 23/01/2017.
  */
-public class ChannelListActivity extends Activity implements OnDownloadCompleteListener, AdapterView.OnItemClickListener {
+public class ChannelListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private TextView txt;
     private ListView lvChans;
@@ -34,20 +30,6 @@ public class ChannelListActivity extends Activity implements OnDownloadCompleteL
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channelist);
-        //lecture des shared prefs
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String token = settings.getString("accessToken", "rien");
-        //récupération des channels
-        Downloader d = new Downloader("http://www.raphaelbischof.fr/messaging/?function=getchannels",token);
-        d.addOnDownloadCompleteListener(this);
-        d.execute();
-        txt = (TextView) findViewById(R.id.tv1);
-        lvChans = (ListView) findViewById(R.id.lvchans);
-        //ArrayAdapter ça marche mais une seule donnée affichée ...
-        //lvChans.setAdapter(new ArrayAdapter(getApplicationContext(), R.layout.layout_simple_chan,R.id.textView, chanels));
-        //expérimental (recréer un arrayadapter) a faire quand t'auras le temps ou l'energie parce que ça te soule
-        lvChans.setAdapter(new MyshatedArrayAdapter(this.getApplicationContext(), chanels));
-        lvChans.setOnItemClickListener(this);
     }
 
 
@@ -67,26 +49,19 @@ public class ChannelListActivity extends Activity implements OnDownloadCompleteL
         finish();
     }
 
-    @Override
-    public void onDownloadComplete(String content, int requestCode) {
-        Gson gson = new Gson();
-        Channels chans = gson.fromJson(content,Channels.class);
-        for(Channel chan : chans.getChannels())
-        {
-            chanels.add(chan);
-        }
-        lvChans.setAdapter(new MyshatedArrayAdapter(this.getApplicationContext(), chanels));
-    }
-
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent myIntent = new Intent(getApplicationContext(), ChannelActivity.class);
-        //Toast.makeText(getApplicationContext(), chanels.get(position).toString(),Toast.LENGTH_SHORT).show();
-        Channel chan = chanels.get(position);
-        //Toast.makeText(getApplicationContext(), chan.getChannelID()+"" ,Toast.LENGTH_SHORT).show();
-        myIntent.putExtra("chanID", chan.getChannelID());
-        startActivityForResult(myIntent, 1);
+        ChannelListFragment fragA = (ChannelListFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentA_ID);
+        MessageFragment fragB = (MessageFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentB_ID);
+        Channel chan = fragA.chanels.get(position);
+        if(fragB == null|| !fragB.isInLayout()){
+            Intent i = new Intent(getApplicationContext(),ChannelActivity.class);
+            i.putExtra("chanID", chan.getChannelID());
+            startActivity(i);
+        } else {
+            fragB.fillTextView();
+        }
     }
 
     @Override
--- app/src/main/java/fragmentPackage/ChannelListFragment.java
+++ app/src/main/java/fragmentPackage/ChannelListFragment.java
@@ -1,6 +1,7 @@
 package fragmentPackage;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,31 +9,62 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import alexis.boulet.channelmessaging.Channel;
+import alexis.boulet.channelmessaging.ChannelListActivity;
+import alexis.boulet.channelmessaging.Channels;
+import alexis.boulet.channelmessaging.Downloader;
+import alexis.boulet.channelmessaging.MyshatedArrayAdapter;
+import alexis.boulet.channelmessaging.OnDownloadCompleteListener;
 import alexis.boulet.channelmessaging.R;
 
 /**
  * Created by bouleta on 27/02/2017.
  */
-public class ChannelListFragment extends Fragment {
+public class ChannelListFragment extends Fragment implements OnDownloadCompleteListener {
     private ListView lvchanfrags;
-    public String[] listItems = {"item 1", "item 2 ", "list", "android", "item 3", "foobar", "bar", };
+    public List<Channel> chanels = new ArrayList<Channel>();
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private TextView txt;
+    private ListView lvChans;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.channel_list_fragment,container);
         lvchanfrags = (ListView)v.findViewById(R.id.lvchanfrags);
+        //lecture des shared prefs
+        SharedPreferences settings = this.getActivity().getSharedPreferences(PREFS_NAME, 0);
+        String token = settings.getString("accessToken", "rien");
+        //récupération des channels
+        Downloader d = new Downloader("http://www.raphaelbischof.fr/messaging/?function=getchannels",token);
+        d.addOnDownloadCompleteListener(this);
+        d.execute();
+        txt = (TextView) v.findViewById(R.id.tv1);
+        lvChans = (ListView) v.findViewById(R.id.lvchanfrags);
         return v;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        lvchanfrags.setAdapter(new ArrayAdapter(getActivity(), R.layout.channel_list_fragment, listItems));
-        lvchanfrags.setOnItemClickListener((MainActivity)getActivity());
+        lvchanfrags.setAdapter(new ArrayAdapter(getActivity(), R.layout.channel_list_fragment, chanels));
+        lvchanfrags.setOnItemClickListener((ChannelListActivity)getActivity());
+    }
+
+    @Override
+    public void onDownloadComplete(String content, int requestCode) {
+        Gson gson = new Gson();
+        Channels chans = gson.fromJson(content,Channels.class);
+        for(Channel chan : chans.getChannels())
+        {
+            chanels.add(chan);
+        }
+        lvChans.setAdapter(new MyshatedArrayAdapter(this.getActivity().getApplicationContext(), chanels));
     }
 }
