269e7f6ec7d5ae5c004c3c1ca8b84fd345dbff40
kingofzeloose <alexisboulet0@gmail.com>
2017-02-27 11:38:52 +0100

    WIP Fragment : stay layout landscape to make cool

--- app/src/main/java/alexis/boulet/channelmessaging/ChannelActivity.java
+++ app/src/main/java/alexis/boulet/channelmessaging/ChannelActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
-public class ChannelActivity extends Activity implements OnDownloadCompleteListener, View.OnClickListener {
+public class ChannelActivity extends AppCompatActivity implements OnDownloadCompleteListener, View.OnClickListener {
 
     private ListView lvMessage;
     private EditText etMessage;
@@ -27,28 +28,7 @@ public class ChannelActivity extends Activity implements OnDownloadCompleteListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel);
-        final OnDownloadCompleteListener thiss = this;
-        final int chanID = getIntent().getIntExtra("chanID", 0);
-        lvMessage = (ListView) findViewById(R.id.lvMessageReceve);
-        etMessage = (EditText) findViewById(R.id.etTextEnvoi);
-        btnEnvoi = (Button) findViewById(R.id.btnEnvoi);
-        btnEnvoi.setOnClickListener(this);
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        final String token = settings.getString("accessToken", "rien");
-        Downloader d = new Downloader(" http://www.raphaelbischof.fr/messaging/?function=getmessages",token, chanID);
-        d.addOnDownloadCompleteListener(this);
-        d.execute();
-        final Handler  handler = new Handler();
 
-        final Runnable r = new Runnable() {
-            public void run() {
-                Downloader d = new Downloader(" http://www.raphaelbischof.fr/messaging/?function=getmessages",token, chanID);
-                d.addOnDownloadCompleteListener(thiss);
-                d.execute();
-                handler.postDelayed(this, 1000);
-            }
-        };
-        handler.postDelayed(r, 1000);
     }
 
     private void activiteTerminee(boolean resultat, boolean etatHyperactif) {
