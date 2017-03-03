8a0e9eb10a32ab3589a17da9b89257d7c0ca70cf
kingofzeloose <alexisboulet0@gmail.com>
2017-03-03 16:45:08 +0100

    WIP design

--- app/src/main/java/alexis/boulet/channelmessaging/loginActivity.java
+++ app/src/main/java/alexis/boulet/channelmessaging/loginActivity.java
@@ -1,21 +1,32 @@
 package alexis.boulet.channelmessaging;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.gson.Gson;
 
 public class loginActivity extends AppCompatActivity implements View.OnClickListener, OnDownloadCompleteListener {
     private EditText etIdentifiant;
     private EditText etMdp;
     private Button btnValider;
+    protected ImageView mIvLogo;
+    private Handler mHandlerTada;
+    private int mShortDelay;
+    private TextView tvTrans;
     private static final String PREFS_NAME = "MyPrefsFile";
 
 
@@ -26,11 +37,26 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
         etIdentifiant = (EditText) findViewById(R.id.etIdentifiant);
         etMdp = (EditText) findViewById(R.id.etMdp);
         btnValider = (Button) findViewById(R.id.btnValider);
+        mIvLogo = (ImageView) findViewById(R.id.IvLogo);
+        tvTrans = (TextView) findViewById(R.id.tvTrans);
+        mHandlerTada = new Handler(); // android.os.handler
+        mShortDelay = 4000; //milliseconds
+
+        mHandlerTada.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.Tada)
+                        .duration(700)
+                        .playOn(mIvLogo);
+                mHandlerTada.postDelayed(this, mShortDelay);
+            }
+        }, mShortDelay);
         btnValider.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Animation animSlideLeft = AnimationUtils.loadAnimation(this, R.anim.slide_left);
+        tvTrans.startAnimation(animSlideLeft);
         Downloader d = new Downloader(etIdentifiant.getText().toString(),etMdp.getText().toString(),"http://www.raphaelbischof.fr/messaging/?function=connect");
         d.addOnDownloadCompleteListener(this);
         d.execute();
@@ -50,8 +76,8 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
             editor.commit();
             /*Toast toast = Toast.makeText(this.getApplicationContext(), response.toString(), Toast.LENGTH_LONG);
             toast.show();*/
-            Intent myIntent = new Intent(this.getApplicationContext(),ChannelListActivity.class);
-            startActivityForResult(myIntent,0);
+            Intent loginIntent = new Intent(this, ChannelListActivity.class);
+            startActivity(loginIntent, ActivityOptions.makeSceneTransitionAnimation(this, mIvLogo, "logo").toBundle());
         } else {
             Toast toast = Toast.makeText(this.getApplicationContext(), "informations de connexions erronées", Toast.LENGTH_LONG);
             toast.show();
